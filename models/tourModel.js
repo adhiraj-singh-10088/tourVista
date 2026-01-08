9d3359b09ef145296643bae87178e5837ffec4e1
adhiraj-singh-10088 <adhiraj.25bce10088@vitbhopal.ac.in>
2026-01-08 16:11:32 +0530

    Modelled the tours
    added requried and future required fields in the tour model

--- models/tourModel.js
+++ models/tourModel.js
@@ -6,14 +6,56 @@ const tourSchema = new mongoose.Schema({
     required: [true, 'A tour must have a name'],
     unique: true,
   },
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+  },
   price: {
     type: Number,
     required: [true, 'A tour must have a price'],
   },
-  rating: {
+  priceDiscount: {
+    type: Number,
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5,
   },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a summary'],
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image'],
+  },
+  images: {
+    type: [String],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: {
+    type: [Date],
+  }
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
