f8ea7aeacc4d149b1d1256821a807d137fc31752
kzahed610 <kzahed610@gmail.com>
2026-01-09 19:57:44 +0530

    Made it responsive

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -6,7 +6,7 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
+    <div className="content-container">
       <Header />
       {/* FIX: The App component now defines the application's pages using the <Routes> and <Route> components. */}
       {/* WHY: <Routes> is a container that ensures only one route is rendered at a time. Each <Route> acts as a rule that maps a URL path to a specific component. */}
