bc8516351fd0d8151348abb086b903e4db44a9ad
kzahed610 <kzahed610@gmail.com>
2026-01-09 20:14:23 +0530

    Added links to buttons and fixed some colouring

--- Client/src/components/Header.jsx
+++ Client/src/components/Header.jsx
@@ -9,13 +9,18 @@ function Header() {
   return (
     <header className="header-container">
       <div className="header-content">
-        <img src={logo} alt="TourVista Logo" className="header-logo" />
         <nav className="header-nav">
           <a href="/">Home</a>
-          <a href="/tours">Tours</a>
         </nav>
+        <div className="header-logo-container">
+          <img src={logo} alt="TourVista Logo" className="header-logo" />
+        </div>
         <div className="header-actions">
-          <button className="header-signup-btn">Sign Up</button>
+          <a href="/login">
+            <button className="header-signup-btn">
+                Sign Up
+            </button>
+          </a>
           <ThemeToggle isLightMode={isLightMode} setIsLightMode={setIsLightMode} />
         </div>
       </div>
