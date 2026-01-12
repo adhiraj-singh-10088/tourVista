061cf6d1b3fb732e1f9370da01aeb07ec1599743
adhiraj-singh-10088 <adhiraj.25bce10088@vitbhopal.ac.in>
2026-01-12 18:33:16 +0530

    Added monthly tours per year endpoint

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -136,3 +136,36 @@ exports.getTourStats = async (req, res) => {
     });
   }
 };
+
+exports.getMonthlyPlan = async (req, res) => {
+  try {
+    const year = Number(req.params.year);
+    const plan = await Tour.aggregate([
+      {
+        $unwind: '$startDates',
+      },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
+          },
+        },
+      },
+      {
+        $group: {
+          _id: { $month: '$startDates' },
+          numTourStarts: { $sum: 1 },
+          tours: { $push: '$name' },
+        },
+      },
+    ]);
+
+    return res.status(200).json({
+      status: 'success',
+      data: {
+        plan,
+      },
+    });
+  } catch (err) {}
+};
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -22,6 +22,10 @@ router
     .get(tourController.getTourStats);
 
 router
+    .route('/monthly-plan/:year')
+    .get(tourController.getMonthlyPlan);
+
+router
   .route('/:id')
   .get(tourController.getTour)
   .patch(tourController.updateTour)
