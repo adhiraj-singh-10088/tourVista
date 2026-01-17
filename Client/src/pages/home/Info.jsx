011287dbb44f2329fdf9dac3ec2b2b8cefe86dd7
Zahed <kzahed610@gmail.com>
2026-01-17 15:23:09 +0530

    Testing new Mountain SVG

--- Client/src/pages/home/Info.jsx
+++ Client/src/pages/home/Info.jsx
@@ -1,7 +1,11 @@
+import "./Info.css";
+
 function Info() {
     return(
         <div className="infoContainer">
-
+            <div className="information">
+                <h1>test</h1>
+            </div>
         </div>
     );
 }
