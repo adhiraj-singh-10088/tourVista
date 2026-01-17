e1b017d883099a93aec43004a29a57bfdf610e7a
adhiraj-singh-10088 <adhiraj.25bce10088@vitbhopal.ac.in>
2026-01-17 11:23:02 +0530

    updated rejection message in case name and message weren't available

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const server = app.listen(port, () => {
 });
 
 process.on('unhandledRejection', (err) => {
-  console.log(err.name, err.message);
+  console.error('Unhandled Rejection:', err);
   console.log('UNHANDLED REJECTION! Shutting down...');
   server.close(() => {
     process.exit(1);
