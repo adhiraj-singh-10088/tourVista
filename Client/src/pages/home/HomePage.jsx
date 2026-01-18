f278367d3293e56747158d6cd3f783357b8294a0
Zahed <kzahed610@gmail.com>
2026-01-18 12:24:44 +0530

    added clouds

--- Client/src/pages/home/HomePage.jsx
+++ Client/src/pages/home/HomePage.jsx
@@ -1,3 +1,5 @@
+import { useRef } from "react";
+import { useInView } from "framer-motion";
 import ResponsiveImage from "./ResponsiveImage.jsx";
 import Title from "./Title.jsx";
 import Mountain from "./Mountain.jsx";
@@ -5,27 +7,45 @@ import Info from "./Info.jsx";
 
 import TourVista3 from "../../assets/TourVista3.png";
 import TourVistaBG from "../../assets/TourVistaBG.png";
+import CloudsIMG from '../../assets/CloudsIMG.png';
+import LeavesIMG from '../../assets/LeavesIMG.png';
 
 import "./HomePage.css";
 
 function HomePage() {
+  const heroRef = useRef(null);
+  const isHeroInView = useInView(heroRef, { amount: 0.01 });
+
   return (
     <div className="scroll-container">
 
       {/* HERO SECTION */}
-      <section className="page hero-container">
-        <ResponsiveImage
-          lowSrc={TourVista3}
-          highSrc={TourVistaBG}
-          alt="Hero image"
-          priority={true}
-        />
-        <Title />
-        <Mountain />
+      <section className="page hero-container" ref={heroRef}>
+        {isHeroInView && (
+          <>
+            <ResponsiveImage
+              lowSrc={TourVista3}
+              highSrc={TourVistaBG}
+              alt="Hero image"
+              priority={true}
+            />
+            <Title />
+            <Mountain />
+          </>
+        )}
       </section>
 
       {/* INFO SECTION */}
-      <section className="page">
+      <section className="page info-section">
+        <div className="section-bridge">
+          <img 
+            className="clouds-img"
+            src={CloudsIMG} 
+            alt="CloudsIMG" />
+        </div>
+
+        <img src={LeavesIMG} alt="LeavesIMG" />
+
         <Info />
       </section>
 
